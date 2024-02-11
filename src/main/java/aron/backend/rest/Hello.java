e0ad97f37050000d7d12346cd0411c4f3202a7b2
Aron Gulyas <aron.gulyas.hu@gmail.com>
2024-02-11 14:24:49 +0100

    save() -- playground -- 2024-02-11 14:24:49

--- src/main/java/aron/backend/rest/Hello.java
+++ src/main/java/aron/backend/rest/Hello.java
@@ -1,26 +1,49 @@
 package aron.backend.rest;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.util.List;
 
 @Path("/hello")
 @Produces(MediaType.APPLICATION_JSON)
 public class Hello {
-
     @GET
-    public String helloRoot(){
+    public String helloRoot() {
         return "Hello from /hello!";
     }
 
     @GET
     @Path("/{name}")
-    public Response sayHello( @PathParam("name") final String msg ) {
-        final String output = "{ \"message\": \"Hello, " + msg + "!\" }";
-        return Response.status(200).entity(output).build();
+    public Response helloPathParam( @PathParam("name") final String name ) {
+        final Message message = new Message("Hello " + name + "!");
+        return Response.status(200).entity(message).build();
+    }
+
+    @GET
+    @Path("/query")
+    public Response helloQueryParam( @QueryParam("from") final String from,
+                                     @QueryParam("to") final String to ) {
+
+        return Response.status(200)
+                       .entity(new Message("from= " + from + ", to= " + to))
+                       .build();
+    }
+
+    @GET
+    @Path("/queries")
+    public Response helloQueryList( @QueryParam("list") List <String> list) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for ( int i = 0; i < list.size(); i++ ) {
+            stringBuilder.append(list.get(i));
+            if ( i < list.size() -1 ) {
+                stringBuilder.append(" ");
+            }
+        }
+
+        return Response.status(200)
+                       .entity(new Message("list= " + stringBuilder))
+                       .build();
     }
 }
---
+++ src/main/java/aron/backend/rest/Message.java
@@ -0,0 +1,4 @@
+package aron.backend.rest;
+
+public record Message(String message) {
+}
